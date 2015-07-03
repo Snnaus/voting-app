c34ab20fb8b3787a4f38ee4b3be01cdf40361558
snnaus <nausleyg@gmail.com>
2015-07-03 18:26:01 +0000

    Got the update to the server to work

--- client/app/showPoll/showPoll.controller.js
+++ client/app/showPoll/showPoll.controller.js
@@ -3,32 +3,45 @@
 angular.module('workspaceApp')
   .controller('ShowPollCtrl', function ($scope, $http, $routeParams, Auth, $log) {
     $scope.$log = $log;
+    $scope.voted = false;
     $http.get('/api/polls/'+$routeParams.id).success(function(poll){
       $scope.poll = poll;
-      var labels = [];
-      var datapts = [];
-      console.log(poll.choices);
-      for(var i in poll.choices){
-        labels.push(i);
-        datapts.push(poll.choices[i]);
-      }
       
-      var data = {
-        labels: labels,
-        datasets: [{data: datapts}]
-      };
       
-      var ctx = document.getElementById('bar').getContext('2d');
       
-      var newChart = new Chart(ctx).Bar(data, {
-        responsive: true,
-      });
       
       $scope.userChoice = {};
       
+      
+      
+      
     });
     
-    
+    $scope.voting = function(){
+        var labels = [];
+        var datapts = [];
+        for(var i in $scope.poll.choices){
+          labels.push(i);
+          datapts.push($scope.poll.choices[i]);
+        }
+        
+        $scope.voted = true;
+        var data = {
+          labels: labels,
+          datasets: [{data: datapts}]
+        };
+      
+        var ctx = document.getElementById('bar').getContext('2d');
+      
+         var newChart = new Chart(ctx).Bar(data, {
+          responsive: false,
+          }); 
+        
+        $http.put('/api/polls/'+$routeParams.id, $scope.poll);
+        $http.get('/api/polls/'+$routeParams.id).success(function(poll){
+          console.log(poll);
+        });
+      };
     
     /*for(var i in $scope.poll.choices){
       $scope.labels.push(i);
--- server/api/poll/poll.controller.js
+++ server/api/poll/poll.controller.js
@@ -35,6 +35,7 @@ exports.update = function(req, res) {
     if (err) { return handleError(res, err); }
     if(!poll) { return res.send(404); }
     var updated = _.merge(poll, req.body);
+    updated.markModified('choices');
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
       return res.json(200, poll);
